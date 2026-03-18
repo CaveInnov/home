d31fcdd18a6da957bf4df60419cb3b643f12d179
CaveInnov <caveinnov@gmail.com>
2026-03-18 23:10:13 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+function scrollToSection(id){
+document.getElementById(id).scrollIntoView({behavior:'smooth'});
+}
+document.getElementById("contactForm").addEventListener("submit",function(e){
+e.preventDefault();
+document.getElementById("formMessage").innerText="Message sent!";
+});
